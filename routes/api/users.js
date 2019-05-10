ea4ee66090d5f90cdc2e39cab555beb140a61973
Admin <admin@MacBook-Pro-de-Admin.local>
2019-05-10 06:18:55 +0200

    JWT done

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require ('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -50,9 +52,22 @@ router.post(
 
     await user.save();
 
-    // return the jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
 
-    res.send('User registered'); 
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 } ,
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      }
+    );
+
     } catch(err) {
       console.log(err.message);
       res.status(400).send('Server error');
