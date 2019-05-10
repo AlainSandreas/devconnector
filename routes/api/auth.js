67d27624963ef863f4774b8e5de8546d07925a98
Admin <admin@MacBook-Pro-de-Admin.local>
2019-05-10 10:40:09 +0200

    2 files: check user with email/ password and created the profile schema

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require ('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 // @route  GET api/auth
 // @desc   Test route
@@ -14,6 +18,62 @@ router.get('/', auth, async (req, res) => {
     console.log(err.message);
     res.status(500).send('Server error');
   }
+});
+
+// @route  POST api/auth
+// @desc   Authenticate user & get token
+// @access Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if(!user) {
+        return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+      return res
+      .status(400)
+      .json({ errors: [{ msg: 'Invalid credentials' }] });
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 } ,
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      }
+    );
+
+    } catch(err) {
+      console.log(err.message);
+      res.status(400).send('Server error');
+    }
 });
 
 module.exports = router;
