ee201f8ed8e6852155123713e7593dce0073d6fd
AlainSandreas <sandreas.alain@gmail.com>
2019-06-19 19:34:27 +0200

    display profile > Developers icon

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -50,7 +50,7 @@ export const getProfiles = () => async dispatch => {
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
   try {
-    const res = await axios.get(`/api/profile/user/${userId}`);
+    const res = await axios.get(`/api/profile/${userId}`);
 
     dispatch({
       type: GET_PROFILE,
@@ -59,7 +59,7 @@ export const getProfileById = userId => async dispatch => {
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
+      //payload: { msg: err.response.statusText, status: err.response.status } 
     });
   }
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
-const { check, validationResult } = require ('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const bcrypt = require('bcryptjs');
@@ -14,7 +14,7 @@ router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
-  } catch(err) {
+  } catch (err) {
     console.log(err.message);
     res.status(500).send('Server error');
   }
@@ -31,7 +31,7 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
@@ -40,40 +40,40 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
-      if(!user) {
+      if (!user) {
         return res
-        .status(400)
-        .json({ errors: [{ msg: 'Invalid credentials' }] });
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
       }
-    
-    const isMatch = await bcrypt.compare(password, user.password);
 
-    if(!isMatch) {
-      return res
-      .status(400)
-      .json({ errors: [{ msg: 'Invalid credentials' }] });
-    }
+      const isMatch = await bcrypt.compare(password, user.password);
 
-    const payload = {
-      user: {
-        id: user.id
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
       }
-    }
 
-    jwt.sign(
-      payload,
-      config.get('jwtSecret'),
-      { expiresIn: 360000 } ,
-      (err, token) => {
-        if(err) throw err;
-        res.json({ token });
-      }
-    );
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-    } catch(err) {
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
       console.log(err.message);
       res.status(400).send('Server error');
     }
-});
+  }
+);
 
 module.exports = router;
